ea7ad0d071e12daf5d87e2860f677e0e5436d927
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-13 23:19:16 +0200

    add

---
+++ src/types.ts
@@ -0,0 +1,57 @@
+export interface IAvailableCountries {
+  countryCode: string;
+  name: string;
+}
+
+export interface IPopulationCounts {
+  year: string;
+  value: string;
+}
+
+export interface IBorderCountryMain {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: IBorderCountry[] | null;
+}
+
+export interface IBorderCountry {
+  commonName: string;
+  officialName: string;
+  countryCode: string;
+  region: string;
+  borders: null;
+}
+
+export interface IState {
+  availableCountries: IAvailableCountries[];
+  borders: IBorderCountryMain | null;
+  population: IPopulationCounts[];
+  flagUrl: string | null;
+
+  isLoading: boolean;
+  error: unknown | null;
+}
+
+export type AvailableCountriesPayload = IAvailableCountries[];
+
+export interface InfoPayload {
+  borders: IBorderCountryMain | null;
+  population: IPopulationCounts[];
+  flagUrl: string;
+}
+
+export interface ICountryChartProps {
+  population: IPopulationCounts[];
+}
+
+export interface IBordersProps {
+  borders: IBorderCountry[] | null;
+}
+
+export interface IInfoProps {
+  flagUrl: string | null;
+  title?: string;
+  countryCode: string;
+}
