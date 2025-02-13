4f2a8defd72994c5348ce7ef88da5f241e4ca6a5
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-13 23:18:57 +0200

    add

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import "./scss/index.scss";
+
+import { SharedLayout } from "./components/SharedLayout";
+import { lazy } from "react";
+
+const CountryPage = lazy(() => import("./pages/CountryPage"));
+const InfoPage = lazy(() => import("./pages/InfoPage"));
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route path="/" index element={<CountryPage />} />
+          <Route path="country-info-page/:countryCode" element={<InfoPage />} />
+
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
