f8f97b7d98ff66df0dac36d2d0df467b1d84d803
KaterynaBachkalo <kozlovets88@gmail.com>
2025-02-18 13:29:24 +0200

    add ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
 import { createRoot } from 'react-dom/client';
-// import "./index.css";
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store.ts';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from './contexts/ThemeProvider.tsx';
+import { createContext } from 'react';
+import { IThemeContext } from './types.ts';
+
+export const ThemeContext = createContext<IThemeContext | undefined>(undefined);
 
 createRoot(document.getElementById('root')!).render(
   <>
     <Provider store={store}>
       <BrowserRouter basename="/countries-info-app-frontend">
-        <App />
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </Provider>
   </>,
